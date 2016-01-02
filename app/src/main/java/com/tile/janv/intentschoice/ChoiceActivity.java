214be0435b605bc1bf248accad27279c0d72ec4b
janv <janvenstermans@gmail.com>
2016-01-02 20:17:26 +0100

    add google search dialog

--- app/src/main/java/com/tile/janv/intentschoice/ChoiceActivity.java
+++ app/src/main/java/com/tile/janv/intentschoice/ChoiceActivity.java
@@ -1,12 +1,17 @@
 package com.tile.janv.intentschoice;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.UnsupportedEncodingException;
@@ -18,6 +23,7 @@ import butterknife.OnClick;
 public class ChoiceActivity extends AppCompatActivity {
 
     public static final String WEBPAGE = "http://tinfbo2.hogent.be/nativeapps/index.php";
+    public static final String LOG_TAG = "ChoiceActivity";
 
     //---------------------
     // lifecyle methods
@@ -62,40 +68,52 @@ public class ChoiceActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_open_web_page)
     protected void onOpenWebPage() {
+        Log.i(LOG_TAG, "Clicked button to open web page.");
         Intent openWebPageIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(WEBPAGE));
         tryStartingIntent(openWebPageIntent, R.string.open_web_page_fail);
     }
 
     @OnClick(R.id.button_open_contacts)
     protected void onOpenContacts() {
+        Log.i(LOG_TAG, "Clicked button to open contacts.");
         Intent openContactsIntent = new Intent(Intent.ACTION_VIEW,  ContactsContract.Contacts.CONTENT_URI);
         tryStartingIntent(openContactsIntent, R.string.open_contacts_fail);
     }
 
     @OnClick(R.id.button_open_dialer)
     protected void onOpenDialer() {
+        Log.i(LOG_TAG, "Clicked button to open dialer.");
         Intent openDialerIntent = new Intent(Intent.ACTION_DIAL);
         tryStartingIntent(openDialerIntent, R.string.open_dialer_fail);
     }
 
     @OnClick(R.id.button_search_google)
     protected void onSearchGoogle() {
-        try {
-            //http://stackoverflow.com/questions/13853375/how-to-search-a-string-on-google-using-default-browser
-            String keywords = "how to make mayonaise";
-            String query = URLEncoder.encode(keywords, "utf-8");
-            String url = "http://www.google.com/search?q=" + query;
-            Intent searchGoogleIntent = new Intent(Intent.ACTION_VIEW);
-            searchGoogleIntent.setData(Uri.parse(url));
-            tryStartingIntent(searchGoogleIntent, R.string.search_google_fail);
-        } catch (UnsupportedEncodingException e) {
-            Toast.makeText(this, getString(R.string.search_google_string_encoding_fail), Toast.LENGTH_SHORT).show();
-        }
-
+        Log.i(LOG_TAG, "Clicked button to search google.");
+        //http://stackoverflow.com/questions/18799216/how-to-make-a-edittext-box-in-a-dialog
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle(getString(R.string.search_google_dialog_title));
+        alertDialog.setMessage(getString(R.string.search_google_dialog_message));
+        final EditText input = new EditText(ChoiceActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+        alertDialog.setPositiveButton(getString(R.string.search_google_dialog_button),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        String keywords = input.getText().toString();
+                        Log.i(LOG_TAG, "Keywords entered to search in google: " + keywords);
+                        searchGoogle(keywords);
+                    }
+                });
+        alertDialog.show();
     }
 
     @OnClick(R.id.button_voice_command)
     protected void startVoiceCommand() {
+        Log.i(LOG_TAG, "Clicked button to start voice command.");
         Intent intent = new Intent(Intent.ACTION_VOICE_COMMAND);
         tryStartingIntent(intent, R.string.voice_command_fail);
     }
@@ -109,6 +127,19 @@ public class ChoiceActivity extends AppCompatActivity {
             startActivity(intent);
         } else {
             Toast.makeText(this, getString(failStringId), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void searchGoogle(String keywords) {
+        try {
+            //http://stackoverflow.com/questions/13853375/how-to-search-a-string-on-google-using-default-browser
+            String query = URLEncoder.encode(keywords, "utf-8");
+            String url = "http://www.google.com/search?q=" + query;
+            Intent searchGoogleIntent = new Intent(Intent.ACTION_VIEW);
+            searchGoogleIntent.setData(Uri.parse(url));
+            tryStartingIntent(searchGoogleIntent, R.string.search_google_fail);
+        } catch (UnsupportedEncodingException e) {
+            Toast.makeText(this, getString(R.string.search_google_string_encoding_fail), Toast.LENGTH_SHORT).show();
         }
     }
 }
